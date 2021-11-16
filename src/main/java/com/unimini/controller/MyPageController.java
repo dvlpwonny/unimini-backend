a62d90bd66c41af0f019c4dc979243d732bdf95f
dvlpwonny <jeongwon826@gmail.com>
2021-11-16 09:31:04 +0900

    유니티테스트

--- src/main/java/com/unimini/controller/MyPageController.java
+++ src/main/java/com/unimini/controller/MyPageController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
@@ -22,10 +19,11 @@ public class MyPageController {
     @Autowired
     MyPageService myPageService;
 
-    @GetMapping(value = "/myPage/myPageForm")
-    public ModelAndView myPage(@RequestBody Map<String, String> paramMap) {
+    @RequestMapping(value = "/myPage/myPageForm", method = {RequestMethod.GET, RequestMethod.POST})
+    public ModelAndView myPage(@RequestParam String userId) {
         ModelAndView mav = new ModelAndView("myPage");
-        /*Map<String, String> paramMap = new HashMap<>();*/
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("userId", userId);
 
         /*paramMap.put("userId", principal.getName());*/
         Map<String, String> userInfo = myPageService.getUserInfo(paramMap);
